f7c88068fa609ca4e80a6d7945a778a724d3bf0a
RCmags <memoryofatrufestival@gmail.com>
2022-09-27 15:21:48 -0500

    nano version

---
+++ nano/controller-aoa-sensor/controller-aoa-sensor.ino
@@ -0,0 +1,183 @@
+/* FLYING WING CONTROLLER */
+
+/* Arduino NANO sketch to stabilize an RC flying wing controlled by weight-shift or trailing edge flaps. 
+ * It commands two servos and applies V-tail mixing to stabilize the pitch axis.
+ * Author: RCmags https://github.com/RCmags
+*/
+
+//=============== Connections ================
+// See included schematic
+  // Inputs:
+// Pin 8 -> Receiver CH1
+// Pin 9 -> Receiver CH2
+// Pin A7 -> Analog sensor
+  // Outputs:
+// Pin 6  -> Left  wing servo
+// Pin 7  -> Right wing servo 
+
+//=================== Code ===================
+#include <Servo.h>
+#include "parameters.cpp"
+
+//----- constants
+#define SENSOR_PIN    A7
+
+//----- global variables 
+Servo servo[2]; 
+volatile uint16_t pwm_input[2] = {0};
+
+//----- Input signals
+// PORTB = {8 .. 13} -> using pins {8 .. 9} = B00000011
+
+/* port change interrupt to read PWM inputs from receiver */
+ISR( PCINT0_vect ) {
+  static uint32_t initial_time[2] = {0}; 
+  static uint8_t port_last = PINB;  
+  
+  // port changes
+  uint32_t current_time = micros(); 
+  uint8_t port_rise = ~port_last & PINB;
+  uint8_t port_fall = port_last & ~PINB;
+  
+  // find changing pins
+  for( uint8_t index = 0; index < 2; index += 1) {
+    uint8_t mask = B00000001 << index;  // Start at PCINT0
+    if( port_rise & mask ) {                
+        initial_time[index] = current_time;
+    } else if ( port_fall & mask ) {       
+        pwm_input[index] = current_time - initial_time[index];
+    }
+  }
+  port_last = PINB;    
+}
+
+void setupISR() {
+  // enable PORTB interrupts  
+  PCICR |= (1 << PCIE0);
+  // enable PCINT for pins 8-9     
+  PCMSK0 |= (1 << PCINT0);   
+  PCMSK0 |= (1 << PCINT1);
+  // set as input
+  pinMode(8, INPUT_PULLUP);   
+  pinMode(9, INPUT_PULLUP); ; 
+}
+
+//----- Input filter
+
+/* scale and center inputs */
+void scaleInputs(float* output) {
+  output[0] = float( int(pwm_input[0]) - PWM_MID )*GAIN_ROLL;    // roll
+  output[1] = float( int(pwm_input[1]) - PWM_MID )*GAIN_PITCH;   // pitch
+}
+
+//----- Analog sensor
+
+/* read sensor and calibrate to degrees */
+float readSensor() {
+  constexpr float SLOPE = float(ANGLE_MAX - ANGLE_MIN) / float(ANALOG_MAX - ANALOG_MIN);
+  constexpr int   CONST = float(ANALOG_MAX + ANALOG_MIN)/2 + ANALOG_OFFSET;
+  // scale reading
+  float value = analogRead(SENSOR_PIN) - CONST;     
+  return SLOPE*value;
+}
+
+/* remove noise from analog sensor and calculate derivative */
+void filterSensor(float *xbar, float *dxdt) {
+  // store state
+  static uint32_t last_time = millis();
+  static float x[2] = {0}, v = 0;
+
+  // time change
+  uint32_t curr_time = micros();
+  float dt  = float(curr_time - last_time)*1e-6;
+  last_time = curr_time; 
+
+  // alpha-beta filter:
+  constexpr float BETA = ALPHA * ALPHA * 0.25;
+    // error
+  float xi = readSensor();
+  float dx = 0.5*( xi - x[1] );
+    // position
+  float xnew = x[0];
+  xnew += v*dt;  
+  xnew += ALPHA * dx; 
+    // velocity
+  v += BETA * dx/dt;
+
+  // store past position
+  x[1] = x[0];
+  x[0] = xnew;
+
+  // return smoothed value and derivative
+  *xbar = xnew;
+  *dxdt = v;
+}
+
+//----- PID controller
+
+#ifdef USING_WEIGHT_SHIFT
+
+/* gain to maintain constant torque indifferent of angle of attack */
+float gainAngle(float angle) {
+  return angle == 0 ? 0 : AOA_TRIM/angle;
+}
+#endif
+
+/* PID controller to stabilize pitch axis */
+float PIDcontroller(float input) {
+  float angle; float angle_deriv; filterSensor(&angle, &angle_deriv);
+  
+  // desired angle of attack
+  constexpr float SCALE = 5.0 / 500.0;     // Convert 500 ms to 5 degrees
+  
+  float targ_angle = SCALE*input + AOA_TRIM;
+  targ_angle = constrain(targ_angle, AOA_MIN, AOA_MAX);
+  
+  // use P and D terms
+  float output = GAIN_PROP*(angle - targ_angle) + GAIN_DERIV*angle_deriv;
+  
+  #ifdef USING_WEIGHT_SHIFT
+    return output * gainAngle(angle); 
+  #else
+    return output;
+  #endif
+}
+
+//----- Servos
+
+void setupServos() {    
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  servo[0].attach(6);
+  servo[1].attach(7);
+}
+
+//----- Main loop
+
+void setup() {
+  setupISR();
+  setupServos();
+  pinMode(SENSOR_PIN, INPUT_PULLUP); // sensor
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // combine inputs
+  float input[2]; scaleInputs(input);
+  
+  float output = PIDcontroller( -input[1] );  // pitch input controls target AoA
+  float mix1 = input[0] + output;
+  float mix2 = input[0] - output;
+  
+  // command servos
+  mix1 += PWM_MID + TRIM_LEFT;
+  mix2 += PWM_MID + TRIM_RIGHT;
+  
+  mix1 = constrain(mix1, PWM_MIN, PWM_MAX );
+  mix2 = constrain(mix2, PWM_MIN, PWM_MAX );
+
+  servo[0].writeMicroseconds( mix1 );    // left wing
+  servo[1].writeMicroseconds( mix2 );    // right wing
+  Serial.println( analogRead(SENSOR_PIN) );
+}
--- nano/controller-aoa-sensor/parameters.cpp
+++ nano/controller-aoa-sensor/parameters.cpp
@@ -12,12 +12,8 @@
 #define PWM_MID           1500    // us     // Center/middle servo position
 #define PWM_MIN           1000    // us     // Minimum servo position
 #define PWM_MAX           2000    // us     // Maximum servo position
-
-// 4. Signal filters
-#define INPUT_CHANGE      12      // us     // Change in PWM signal needed to update receiver inputs
-#define ALPHA             0.05              /* Gain of alpha-beta filter applied to sensor input. 
-                                               A smaller value smoothens the reading at the cost of slower response */
-// 5. Pitch Stabilization
+                                            
+// 4. Pitch Stabilization
   // 1. PID Controller
 #define GAIN_PROP         40.0              // Proportional gain. Adjusts spring response
 #define GAIN_DERIV        0.0               // Derivative gain. Adjusts damping response
@@ -26,13 +22,16 @@
 #define AOA_MIN           1.0     // deg    // Minimum target angle of attack. Prevents wings unloading
 #define AOA_MAX           15.0    // deg    // Maximum taget angle of attack. Prevents stall
                                             /* NOTE: AOA_MIN < AOA_TRIM < AOA_MAX */
-// 6. Sensor calibrationg
-#define ANGLE_MAX         95      // deg    // Largest deflection of angle sensor 
-#define ANGLE_MIN         -95     // deg    // Smallest deflection of angle sensor
+// 5. Sensor filter
+#define ALPHA             0.05              /* Gain of alpha-beta filter applied to sensor input. 
+                                            A smaller value smoothens the reading at the cost of slower response */
+// 6. Sensor calibration
+#define ANGLE_MAX         90      // deg    // Largest deflection of angle sensor 
+#define ANGLE_MIN         -90     // deg    // Smallest deflection of angle sensor
                                             /* NOTE: ANGLE_MIN < ANGLE_MAX */
-#define ANALOG_MAX        750               // AnalogRead output at largest deflection
-#define ANALOG_MIN        250               // AnalogRead output at smallest deflection
-#define ANALOG_OFFSET     220               // Offset to align sensor with zero-lift angle of wing
+#define ANALOG_MAX        870               // AnalogRead output at largest deflection
+#define ANALOG_MIN        180               // AnalogRead output at smallest deflection
+#define ANALOG_OFFSET     0                 // Offset to align sensor with zero-lift angle of wing
 
 // 7. Settings
 //#define USING_WEIGHT_SHIFT                  // Uncomment for weight-shift pitch control.  
